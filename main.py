4cc9407e6691cca7e703c30e7b5087be7b986747
S.Seymen <sseymen@poliport.com>
2026-06-25 05:17:52 +0000

    Fix: selenium.webdriver.common.by import düzeltildi + collect_all selenium eklendi

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.by import By
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import (
